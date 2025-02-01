d081f942f5e5be80ece2cdeb7edbda7daa60206c
msk <msky0912@gmail.com>
2025-02-01 22:20:57 +0900

    API 예외 처리 - HandlerExceptionResolver 활용
     - 예외발생시 WAS 까지 예외가 던져지고, WAS 에서 오류페이지 정보를 찾아서 다시 /error 를 호출하는 복잡한 과정을 바로 처리할 수 있게 수정
     - UserException 생성
     - ApiExceptionController 에서 UserException 발생하도록 추가
     - UserException 을 처리하는 UserHandlerExceptionResolver 생성
     HTTP 요청 헤더의 accept 값이 application/json 이면 JSON, 그 외에는 error/500 에 있는 HTML 오류페이지를 보여주도록 처리
     - WebConfig 에 UserHandlerExceptionResolver 추가
     - ExceptionResolver 를 사용하면 컨트롤러에서 예외가 발생해도 바로 처리할 수 있다. 따라서 서블릿 컨테이너까지 예외가 전달되지 않고, 스프링 MVC 에서 예외처리가 끝난다.
      서블릿 컨테이너까지 예외가 올라가서 지저분한 추가 프로세스가 실행되는 것을 막을 수 있다.

---
+++ src/main/java/hello/exception/resolver/UserHandlerExceptionResolver.java
@@ -0,0 +1,50 @@
+package hello.exception.resolver;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.exception.exception.UserException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+public class UserHandlerExceptionResolver implements HandlerExceptionResolver {
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        try {
+            if (ex instanceof UserException) {
+                log.info("UserException resolver to 400");
+                String acceptHeader = request.getHeader("accept");
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+
+                //HTTP 요청헤더의 ACCEPT 값이 application/json 이면 JSON 으로 오류
+                if ("application/json".equals(acceptHeader)) {
+                    Map<String, Object> errorResult = new HashMap<>();
+                    errorResult.put("ex", ex.getClass());
+                    errorResult.put("message", ex.getClass());
+                    String result = objectMapper.writeValueAsString(errorResult);
+
+                    response.setContentType("application/json");
+                    response.setCharacterEncoding("utf-8");
+                    response.getWriter().write(result);
+                    return new ModelAndView();
+                } else {
+                    // TEXT/HTML: application/json 이 아니면 error/500 에 있는 HTML 오류페이지를 보여준다.
+                    return new ModelAndView("error/500");
+                }
+            }
+        } catch (IOException e) {
+            log.error("resolver ex", e);
+        }
+        return null;
+    }
+}
