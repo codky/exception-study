0d5defe1003de2b916ede88712316ccd042bb03a
minseok kyeon <msky0912@gmail.com>
2025-02-03 11:16:47 +0900

    API 예외 처리: @ExceptionHandler
     - 예외 발생시 API 응답으로 사용할 객체 정의: ErrorResult
     - @ExceptionHandler 예외처리:
      1. @ExceptionHandler 어노테이션을 선언하고, 해당 컨트롤러에서 처리하고 싶은 예외를 지정하면 예외가 발생했을 때 메서드가 호출되고, 부모, 자식 클래스가 있다면 자식클래스까지 처리 가능->@ExceptionHandler(IllegalArgumentException.class) illegalExHandle(IllegalArgumentException e)
    
       실행 흐름
       - 컨트롤러를 호출한 결과 IllegalArgumentException 예외가 컨트롤러 밖으로 던져진다.
       - 예외가 발생했으므로 ExceptionResolver 가 작동하고, 가장 우선순위가 높은 ExceptionHandlerExceptionResolver 가 실행된다.
       - ExceptionHandlerExceptionResolver 는 해당 컨트롤러에 IllegalArgumentException 을 처리할 수 있는 @ExceptionHandler 가 있는지 확인한다.
       - illegalExHandle() 를 실행한다. @RestController 이므로 illegalExHandle() 에도 @ResponseBody 가 적용된다. 따라서 HTTP 컨버터가 사용되고, 응답이 다음과 같은 JSON 으로 반환된다.
       - @ResponseStatus(HttpStatus.BAD_REQUEST) 를 지정했으므로 HTTP 상태 코드 400으로 응답한다.
    
      2. 예외 생략: @ExceptionHandler 에 예외를 생략할 수 있음. 생략하면 메서드 파라미터의 예외가 지정됨->@ExceptionHandler userExHandle(UserException e)
    
       실행 흐름
       - @ExceptionHandler 에 예외를 지정하지 않으면 해당 메서드 파라미터 예외를 사용한다.(UserException)
       - ResponseEntity 를 사용해서 HTTP 메시지 바디에 직접 응답한다. 물론 HTTP 컨버터가 사용된다.
       - ResponseEntity 를 사용하면 HTTP 응답 코드를 프로그래밍해서 동적으로 변경할 수 있다. 앞에서 @ResponseStatus 는 어노테이션이므로 HTTP 응답코드를 동적으로 변경할 수 없다.
    
      3. Exception 처리: exHandle()
    
       처리 흐름
       - throw new RuntimeException("잘못된 사용자") 코드가 실행되면서, 컨트롤러 밖으로 RuntimeException 이 던져진다.
       - RuntimeException 은 Exception 의 자식 클래스이므로 이 메서드가 호출된다.
       - @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) 로 HTTP 상태 코드를 500 으로 응답한다.

---
+++ src/main/java/hello/exception/exhandler/ErrorResult.java
@@ -0,0 +1,13 @@
+package hello.exception.exhandler;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+//예외가 발생했을 때 API 응답으로 사용할 객체
+@Data
+@AllArgsConstructor
+public class ErrorResult {
+    private String code;
+    private String message;
+
+}
